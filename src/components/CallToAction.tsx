2645bd684232d425f92f5083b66a81ddda9f0390
Kinfe123 <kinfishtech@gmail.com>
2024-06-13 20:35:53 +0300

    fix: minor fixes

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -14,16 +14,7 @@ export function CallToAction() {
       className="relative transform-gpu overflow-hidden   bg-page-gradient pt-32 dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#798ef9_inset]"
     >
       <div className="-z-1 absolute inset-x-0 -top-0 h-screen  w-full bg-transparent bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)]  bg-[size:6rem_4rem] opacity-15 [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
-
-      {/* <Image
-        className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
-        src={backgroundImage}
-        alt=""
-        width={2347}
-        height={1244}
-        unoptimized
-      /> */}
-      <Container className="relative">
+      <Container className="relative z-10">
         <div className="mx-auto max-w-lg text-center">
           <h2 className="font-display text-3xl tracking-tight text-white sm:text-6xl">
             <span className="bg-gradient-to-br from-indigo-400 via-indigo-300 to-indigo-700 bg-clip-text text-transparent">
@@ -36,7 +27,7 @@ export function CallToAction() {
             feel like you’re doing something productive.
           </p>
           <Button
-            className="font-geist text-md mt-3 flex items-center justify-center gap-2 rounded-md bg-gradient-to-br from-indigo-400 to-indigo-700 px-4 py-3 text-center tracking-tighter text-zinc-50 ring-2 ring-indigo-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-indigo-500/70"
+            className="font-geist text-md z-10 mt-3 flex items-center justify-center gap-2 rounded-md bg-gradient-to-br from-indigo-400 to-indigo-700 px-4 py-3 text-center tracking-tighter text-zinc-50 ring-2 ring-indigo-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-indigo-500/70"
             href="/register"
           >
             Get Started with Click
@@ -60,7 +51,7 @@ export function CallToAction() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
             congue, nisl eget molestie varius, enim ex faucibus purus.
           </p>
-          <button className="font-geist z-10 text-md mx-auto mt-5 flex w-fit items-center justify-center gap-2 rounded-md bg-gradient-to-br from-indigo-400  to-indigo-700 px-10 py-2 text-center text-lg tracking-tighter text-zinc-50 ring-2 ring-indigo-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-indigo-500/70">
+          <button className="font-geist text-md z-10 mx-auto mt-5 flex w-fit items-center justify-center gap-2 rounded-md bg-gradient-to-br from-indigo-400  to-indigo-700 px-10 py-2 text-center text-lg tracking-tighter text-zinc-50 ring-2 ring-indigo-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-indigo-500/70">
             Explore more
           </button>
         </div>
