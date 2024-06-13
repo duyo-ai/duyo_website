c251971bffbd74e6ed47a28e997c21f577fc734c
Kinfe123 <kinfishtech@gmail.com>
2024-06-13 20:33:20 +0300

    fix: revamp

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -13,7 +13,7 @@ export function CallToAction() {
       id="get-started-today"
       className="relative transform-gpu overflow-hidden   bg-page-gradient pt-32 dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#798ef9_inset]"
     >
-      <div className="-z-1 absolute inset-x-0 -top-0 h-full  w-full bg-transparent bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)]  bg-[size:6rem_4rem] opacity-15 [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+      <div className="-z-1 absolute inset-x-0 -top-0 h-screen  w-full bg-transparent bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)]  bg-[size:6rem_4rem] opacity-15 [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
 
       {/* <Image
         className="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
@@ -60,7 +60,7 @@ export function CallToAction() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
             congue, nisl eget molestie varius, enim ex faucibus purus.
           </p>
-          <button className="font-geist text-md mx-auto mt-5 flex w-fit items-center justify-center gap-2 rounded-md bg-gradient-to-br from-indigo-400  to-indigo-700 px-10 py-2 text-center text-lg tracking-tighter text-zinc-50 ring-2 ring-indigo-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-indigo-500/70">
+          <button className="font-geist z-10 text-md mx-auto mt-5 flex w-fit items-center justify-center gap-2 rounded-md bg-gradient-to-br from-indigo-400  to-indigo-700 px-10 py-2 text-center text-lg tracking-tighter text-zinc-50 ring-2 ring-indigo-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-indigo-500/70">
             Explore more
           </button>
         </div>
