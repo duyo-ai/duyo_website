1b9b28f4ffea8b4cf3ca05cf5668317f9a9d9000
Kinfe123 <kinfishtech@gmail.com>
2024-06-24 16:38:40 +0300

    fix: comments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -104,32 +104,7 @@ export function Header() {
     <header className="py-10   bg-transparent">
       
       <SlideNavTabs />
-      {/* <Container className=''>
-        <nav className="relative z-50 flex justify-between text-gray-100">
-          <div className="flex items-center md:gap-x-12">
-            <Link href="#" aria-label="Home">
-            </Link>
-            <div className="hidden md:flex md:gap-x-6">
-              <NavLink href="#features">Features</NavLink>
-              <NavLink href="#testimonials">Testimonials</NavLink>
-              <NavLink href="#pricing">Pricing</NavLink>
-            </div>
-          </div>
-          <div className="flex items-center gap-x-5 md:gap-x-8">
-            <div className="hidden md:block">
-              <NavLink href="/login">Sign in</NavLink>
-            </div>
-            <Button href="/register" color="blue">
-              <span>
-                Get started <span className="hidden lg:inline">today</span>
-            </span>
-            </Button>
-            <div className="-mr-1 md:hidden">
-              <MobileNavigation />
-            </div>
-          </div>
-        </nav>
-      </Container> */}
+  
     </header>
   )
 }
